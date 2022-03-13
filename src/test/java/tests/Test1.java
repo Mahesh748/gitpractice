d871f6c08d1e6d50bed5debbd894a070b7a5f90e
Mahesh748 <76986711+Mahesh748@users.noreply.github.com>
2022-03-13 23:39:10 +0530

    Update Test1.java
    changed

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -3,7 +3,7 @@ package tests;
 public class Test1 {
 
 	public static void main(String[] args) {
-		String x="ffs6778skfsn76488757kfsss$#@%^&"abdulkalamsir;
+		String x="ffs6778skfsn76488757kfsss$#@%^&"abdulkalamsir";
 		String y=x.replaceAll("[^0-9]","");
 		System.out.println(y);
 		
